2ccc535a6a87c2c86c4a3fe5c3e8e6e4603381e0
Firrizq <firrizq@gmail.com>
2023-01-25 10:59:33 +0700

    laravel1

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+$tasklist = [
+    'first' => 'sleep',
+    'second' => 'work',
+    'third' => 'play'
+];
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('tes', function () {
+    return view('tes');
+});
+
+Route::get('/hello', function () {
+    return response() -> json ([
+        'message' => "hello world"
+    ]);
+});
+
+Route::get('/tasks', function() use ($tasklist){
+    if (request() -> search) {
+        return $tasklist[request()->search];
+    } else {
+        return $tasklist;
+    }
+});
+
+Route::get('/tasks{param}', function($param) use ($tasklist){
+    return $tasklist[$param];
+});
+
+Route::get('/tasks', function() use ($tasklist){
+    $tasklist[request() -> label] = request()->task;
+    return $tasklist;
+});
+
+Route::patch('/tasks', function() use ($tasklist){
+    $tasklist[request()->key] = request()->task;
+    return $tasklist;
+});
+
+Route::delete('/tasks/{$key}', function($key) use ($tasklist){
+    unset($tasklist[$key]);
+    return $tasklist;
+});
